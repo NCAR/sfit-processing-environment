16d78e29e4c3abf89e2f3dd2bbf08048eb1719f6
Eric Nussbaumer <ebaumer@ucar.edu>
2013-10-29 15:47:40 -0600

    Working through implementation of FilterID filtering. Really should debug before committing

--- ModLib/sfitClasses.py
+++ ModLib/sfitClasses.py
@@ -361,9 +361,13 @@ class DbInputFile(InputFile):
         if not fltDict: fltDict = self.dbInputs
         
         for ind,val in enumerate(fltDict['Flt']):
-            if str(int(val)) == str(fltrID):
-                inds.append(ind)
-        
+            try:
+                if str(int(val)) == str(fltrID):
+                    inds.append(ind)
+            except ValueError:
+                if str(val) == str(fltrID):
+                    inds.append(ind)                
+                
         dbFltInputs = dict((key, [val[i] for i in inds]) for (key, val) in fltDict.iteritems())   # Rebuild filtered dictionary. Syntax compatible with python 2.6
         
         return dbFltInputs
