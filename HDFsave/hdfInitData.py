64480c8b6ea236f0ea10708b0758f48e8bc8ab70
Eric Nussbaumer <ebaumer@ucar.edu>
2014-04-04 15:37:52 -0600

    Move HDFsave project from Dev repo to sfit-processing-environment. Python programs to write GEOMS format HDF files.

---
+++ HDFsave/hdfInitData.py
@@ -0,0 +1,180 @@
+#----------------------------------------------------------------------------------------
+# Name:
+#        hdfInitData.py
+#
+# Purpose:
+#       Class of initializing data. This is the interface between the data source and
+#       the HDF write functions.
+#
+#
+# Notes:
+#      1) Create your own interface to get the data from your source to the HDFfile. 
+#         Currently there are three interfaces in this file:
+#             initIDL -- This interface takes data in from an IDL save file. Note the
+#                         IDL save file has a specific structure
+#             initPy  -- This interface can take data using python functions. It is 
+#                        currently not developed
+#             initDummy -- This is a dummy interface which will create dummy (FillValue)
+#                          data to go into the HDF file.
+#
+# License:
+#    Copyright (c) 2013-2014 NDACC/IRWG
+#    This file is part of sfit4.
+#
+#    sfit4 is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    sfit4 is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with sfit4.  If not, see <http://www.gnu.org/licenses/>
+#
+#----------------------------------------------------------------------------------------
+import datetime as dt
+import numpy as np
+import scipy.io as si
+from itertools import izip
+
+class HDFinitData(object):
+    
+    def initIDL(self,fname):
+        ''' Interface for initializing data with IDL save files'''
+        #--------------------------------------------------------------------------------------------------------------------        
+        #                             -------- IDL Data Structure Variables --------
+        # ['TOTOBS', 'FLTR_NEGVMR', 'FLTR_TC0', 'FLTR_MAXRMS', 'NOBS', 'SPECTRANAME', 'DIRECTORY', 'HHMMSS', 'YYYYMMDD', 
+        #  'SNR', 'P', 'T', 'MS', 'RMS', 'DOFS', 'SZA', 'AZI', 'ITERATIONS', 'ZCORRECT', 'WSHIFT', 'ZEROLEV', 'BSLOPE', 
+        #  'PHASE', 'APRVMR', 'APRLAYCOL', 'APRTC', 'RETVMR', 'RETLAYCOL', 'RETTC', 'H2O_VMR', 'H2O_TC', 'YEAR', 'MONTH', 
+        #  'DAY', 'HRS', 'DOY', 'TYR', 'DATETIME', 'JDAY', 'LATITUDE', 'LONGITUDE', 'ALT_INSTRUMENT', 'SURFACE_PRESSURE', 
+        #  'SURFACE_TEMPERATURE', 'ALT_INDEX', 'ALT_BOUNDARIES', 'ALTITUDE', 'AK', 'AKTC', 'SENS', 'EXTERNAL_SOLAR_SENSOR', 
+        #  'DETECTOR_INTERN_TEMP_SWITCH', 'GUIDER_QUAD_SENSOR_SUM', 'OUTSIDE_RELATIVE_HUMIDITY', 'INT_TIME', 'TOT_RAND_ERR', 
+        #  'TOT_SYS_ERR', 'RAND_COVAR', 'SYS_COVAR']
+        #--------------------------------------------------------------------------------------------------------------------
+        
+        #-------------------------------
+        # Read IDL save file into Python 
+        # dictionary
+        #-------------------------------
+        dataStrc = si.readsav(fname)
+        
+        #------------------------------------------------
+        # Get the name of the variables in data structure
+        # Assuming that the data structure is named ds
+        #------------------------------------------------
+        varName = dataStrc['ds'].dtype.names
+        
+        #----------------------------
+        # Find number of observations
+        #----------------------------
+        nobs = dataStrc['ds']['NOBS'][0]
+        
+        #------------------------------------
+        # Determine number of altitude layers
+        #------------------------------------
+        nlyrs = len(dataStrc['ds']['ALTITUDE'][0])
+                
+        #------------------------------------
+        # Put dates in python datetime object
+        #------------------------------------
+        YYYYMMDD   = dataStrc['ds']['YYYYMMDD']
+        HHMMSS     = dataStrc['ds']['HHMMSS']        
+        self.dates = np.array([dt.datetime(int(ymd[0:4]),int(ymd[4:6]),int(ymd[6:]),int(hms[0:2]),int(hms[2:4]),int(hms[4:])) for (ymd,hms) in izip(YYYYMMDD,HHMMSS)])
+        
+        
+        #---------------------------------------------------
+        # Assign IDL data to attributes to be written to HDF
+        #---------------------------------------------------
+        self.datesJD2K                      = np.asarray(dataStrc['ds']['DATETIME'])
+        self.latitude                       = dataStrc['ds']['LATITUDE'][0]
+        self.longitude                      = dataStrc['ds']['LONGITUDE'][0]
+        self.instAltitudes                  = dataStrc['ds']['ALT_INSTRUMENT'][0] / 1000.0                          # Convert [m] -> [km]
+        self.surfPressures                  = np.asarray(dataStrc['ds']['SURFACE_PRESSURE'])
+        self.surfTemperatures               = np.asarray(dataStrc['ds']['SURFACE_TEMPERATURE'])
+        self.altitudes                      = dataStrc['ds']['ALTITUDE'][0]
+        self.altitudeBoundaries             = np.rot90(dataStrc['ds']['ALT_BOUNDARIES'][0])
+        self.pressures                      = np.vstack(dataStrc['ds']['P']).reshape(nobs,nlyrs)
+        self.temperatures                   = np.vstack(dataStrc['ds']['T']).reshape(nobs,nlyrs)
+        self.gasMxRatAbsSolar               = np.vstack(dataStrc['ds']['RETVMR']).reshape(nobs,nlyrs)
+        self.gasMxRatAbsSolarApriori        = np.vstack(dataStrc['ds']['APRVMR']).reshape(nobs,nlyrs)
+        self.gasMxRatAbsSolarAVK            = np.vstack(dataStrc['ds']['AK']).reshape(nobs,nlyrs,nlyrs) 
+        self.integrationTimes               = np.asarray(dataStrc['ds']['INT_TIME'])  
+        self.gasMxRatAbsSolarUncRand        = np.vstack(dataStrc['ds']['RAND_COVAR']).reshape(nobs,nlyrs,nlyrs)  
+        self.gasMxRatAbsSolarUncSys         = np.vstack(dataStrc['ds']['SYS_COVAR']).reshape(nobs,nlyrs,nlyrs)  
+        self.gasColPartAbsSolar             = np.vstack(dataStrc['ds']['RETLAYCOL']).reshape(nobs,nlyrs)
+        self.gasColPartAbsApriori           = np.vstack(dataStrc['ds']['APRLAYCOL']).reshape(nobs,nlyrs)
+        self.gasColAbsSolar                 = np.asarray(dataStrc['ds']['RETTC']) 
+        self.gasColAbsSolarApriori          = np.asarray(dataStrc['ds']['APRTC'])  
+        self.gasColAbsSolarAVK              = np.vstack(dataStrc['ds']['AKTC']).reshape(nobs,nlyrs) 
+        self.gasColAbsSolarUncRand          = np.asarray(dataStrc['ds']['TOT_RAND_ERR'])  
+        self.gasColAbsSolarUncSys           = np.asarray(dataStrc['ds']['TOT_SYS_ERR'])  
+        self.angleZastr                     = np.asarray(dataStrc['ds']['SZA']) 
+        self.angleSolAz                     = np.asarray(dataStrc['ds']['AZI']) 
+        self.h2oMxRatAbsSolar               = np.vstack(dataStrc['ds']['H2O_VMR']).reshape(nobs,nlyrs) 
+        self.h2oColAbsSol                   = np.asarray(dataStrc['ds']['H2O_TC'])  
+
+    def initPy(self):
+        ''' Interface for initializing data with python set of routines'''
+        
+        #---------------------------------------
+        # Convert dates to Julian Day since 2000
+        #---------------------------------------        
+        self.datesJD2K = np.array([(x - dt.datetime(2000,1,1)).total_seconds()/dt.timedelta(1).total_seconds() for x in self.dates])   
+        
+        
+        
+        
+    def initDummy(self):
+        ''' Interface for initalizing with dummy data. For testing!!!'''
+ 
+        #---------------------------------------
+        # Convert dates to Julian Day since 2000
+        #---------------------------------------        
+        self.dates = np.array([dt.datetime(2008,1,1),dt.datetime(2008,1,2),dt.datetime(2008,1,3),dt.datetime(2008,1,4)])
+        self.datesJD2K = np.array([(x - dt.datetime(2000,1,1)).total_seconds()/dt.timedelta(1).total_seconds() for x in self.dates])   
+                    
+        dummyNlayers                        = 41
+        dummyNangles                        = len(self.dates)
+        dummySingleArray                    = np.empty(1)
+        dummySingleArray.fill(self.getFillValue())
+        dummyLayerData                      = np.empty(dummyNlayers)
+        dummyLayerData.fill(self.getFillValue())
+        dummyAngleData                      = np.empty(dummyNangles)
+        dummyAngleData.fill(self.getFillValue())
+        dummyNotes                          = 'notes about dummy data'
+        dummy2dimLayerData                  = np.empty(2*dummyNlayers).reshape((2, dummyNlayers))
+        dummy2dimLayerData.fill(self.getFillValue())
+        dummy2dimData                       = np.empty(dummyNlayers*dummyNangles).reshape((dummyNangles, dummyNlayers))
+        dummy2dimData.fill(self.getFillValue())
+        dummy3dimData                       = np.empty(dummyNangles*dummyNlayers*dummyNlayers).reshape((dummyNangles, dummyNlayers, dummyNlayers))
+        dummy3dimData.fill(self.getFillValue())
+    
+        self.latitude                       = dummySingleArray
+        self.longitude                      = dummySingleArray
+        self.instAltitudes                  = dummySingleArray
+        self.surfPressures                  = dummyAngleData
+        self.surfTemperatures               = dummyAngleData
+        self.altitudes                      = dummyLayerData
+        self.altitudeBoundaries             = dummy2dimLayerData
+        self.pressures                      = dummy2dimData
+        self.temperatures                   = dummy2dimData
+        self.gasMxRatAbsSolar               = dummy2dimData
+        self.gasMxRatAbsSolarApriori        = dummy2dimData
+        self.gasMxRatAbsSolarAVK            = dummy3dimData
+        self.integrationTimes               = dummyAngleData  
+        self.gasMxRatAbsSolarUncRand        = dummy3dimData  
+        self.gasMxRatAbsSolarUncSys         = dummy3dimData  
+        self.gasColPartAbsSolar             = dummy2dimData
+        self.gasColPartAbsApriori           = dummy2dimData
+        self.gasColAbsSolar                 = dummyAngleData 
+        self.gasColAbsSolarApriori          = dummyAngleData 
+        self.gasColAbsSolarAVK              = dummy2dimData 
+        self.gasColAbsSolarUncRand          = dummyAngleData 
+        self.gasColAbsSolarUncSys           = dummyAngleData 
+        self.angleZastr                     = dummyAngleData
+        self.angleSolAz                     = dummyAngleData
+        self.h2oMxRatAbsSolar               = dummy2dimData 
+        self.h2oColAbsSol                   = dummyAngleData 
