77dc8f3aacfe12980473e2efdcc51ad8d207814a
Mathias <mathias.palm@uni-bremen.de>
2014-04-29 22:17:14 +0200

    Bound zoom in spectrum and show_spectra_by_gas together

--- Lib_MP/show_sfit4_results.py
+++ Lib_MP/show_sfit4_results.py
@@ -37,6 +37,8 @@ class show_results:
         # Find a free figure for AVK
         self.winavk = plt.figure()#figsize=(24,12))
 
+
+
     def __del__(self):
         plt.close(self.winprf)
         plt.close(self.winmw)
@@ -70,7 +72,8 @@ class show_results:
                     ax.legend()
 
             self.winprf.show()
-
+            
+                
         if(type=='mw'):
             self.show_spectra(nr)
 
@@ -83,9 +86,29 @@ class show_results:
 
 
     def show_spectra(self,band_nr = 1,gas=None):
+
+
             self.winmw.clf()
             gs = gridspec.GridSpec(2, 1, height_ratios=[2,1])
+
+
             ax1 = self.winmw.add_subplot(gs[0])
+            ax2 = self.winmw.add_subplot(gs[1])
+
+            self.f = True
+            def oncall1(ax):
+                if not self.f:
+                    self.f = True
+                    ax2.set_xlim(ax.get_xlim())
+                self.f = False
+
+            def oncall2(ax):
+                if not self.f:
+                    self.f = True
+                    ax1.set_xlim(ax.get_xlim())
+                self.f = False
+
+
             ax1.xaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax1.yaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax1.set_autoscaley_on(True)
@@ -93,7 +116,6 @@ class show_results:
             ax1.plot(self.sp.nu[band_nr-1], self.sp.obs[band_nr-1],label='obs')
             ax1.plot(self.sp.nu[band_nr-1], self.sp.clc[band_nr-1],label='clc')
             ax1.legend(bbox_to_anchor=(0.5,1.0), loc=8, ncol=2, fontsize=18)
-            ax2 = self.winmw.add_subplot(gs[1])
             ax2.xaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax2.yaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax2.set_autoscaley_on(True)
@@ -103,32 +125,58 @@ class show_results:
 #            pdb.set_trace()
 #            ax2.set_xlabel(r'Frequency [cm$^{-1}$]')
             self.winmw.show()
+            ax1.callbacks.connect('xlim_changed', oncall1)
+            ax2.callbacks.connect('xlim_changed', oncall2)
+            self.f = False
 
 #            if gas <> None and len(self.gasspectra)>0 :
 #                spec.s
                 
-    def show_spectra_by_gas(self, scan_nr = 1, iter = 'final', band_nr = 1, gas = 'all'):
+    def show_spectra_by_gas(self, scan_nr = 1, itera = -1, band_nr = 1, gas = 'ALL'):
             self.winmw.clf()
             gs = gridspec.GridSpec(2, 1, height_ratios=[2,1])
             ax1 = self.winmw.add_subplot(gs[0])
+            ax2 = self.winmw.add_subplot(gs[1])
 
-            for gas_sp in self.gas.spec[scan_nr][iter][band_nr]:
-                try:
-                    ax1.plot(self.gas.spec[scan_nr][iter][band_nr][gas_sp][0][0], self.gas.spec[scan_nr][iter][band_nr][gas_sp][1][0], label=gas_sp)
-                except:
-                    continue
+            self.f = True
+            def oncall1(ax):
+                if not self.f:
+                    self.f = True
+                    ax2.set_xlim(ax.get_xlim())
+                self.f = False
+
+            def oncall2(ax):
+                if not self.f:
+                    self.f = True
+                    ax1.set_xlim(ax.get_xlim())
+                self.f = False
+
+
+            inds = filter(lambda x: self.gas.band[x]==band_nr \
+                   and self.gas.scan[x]==scan_nr\
+                   and (self.gas.gas[x]==gas or gas == 'ALL')\
+                   and self.gas.iteration[x] == itera,\
+                   range(0,len(self.gas.band)))
+            
+            print inds
+            for ind in inds:
+                ax1.plot(self.gas.nu[ind],self.gas.clc[ind],label=self.gas.gas[ind])
+
             ax1.xaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax1.yaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax1.set_autoscaley_on(True)
             ax1.autoscale_view(True)                      
             ax1.legend(bbox_to_anchor=(1.005, 1), loc=2, borderaxespad=0.)
-            ax2 = self.winmw.add_subplot(gs[1])
+
             ax2.xaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax2.yaxis.set_major_formatter(tkr.ScalarFormatter(useOffset=False))
             ax2.set_autoscaley_on(True)
             ax2.set_autoscalex_on(True)
             ax2.autoscale_view(True)
             ax2.plot(self.sp.nu[band_nr-1], self.sp.dif[band_nr-1])
+            ax1.callbacks.connect('xlim_changed', oncall1)
+            ax2.callbacks.connect('xlim_changed', oncall2)
+            self.f = False
 
 #            pdb.set_trace()
 #            ax2.set_xlabel(r'Frequency [cm$^{-1}$]')
