7df6ab9d4fbd211f130cd2498c46c63c6332f7e4
Mathias Palm <mathias@iup.uni-bremen.de>
2018-09-26 17:46:26 +0200

    test

--- HDFsave/HDFmain_Bre.py
+++ HDFsave/HDFmain_Bre.py
@@ -22,6 +22,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with sfit4.  If not, see <http://www.gnu.org/licenses/>
+#
 #----------------------------------------------------------------------------------------                              
 
 if __name__ != "__main__":
